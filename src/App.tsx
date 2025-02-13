80209083fa8af23069734b1497023ea54293c226
praneeth <gp.gunturu@gmail.com>
2025-02-13 15:10:19 -0600

    Project initiated with Shadcn UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+    return (
+        <div className='text-stone-400'>
+            <Button>Click</Button>
+        </div>
+    )
+}
+
+export default App
